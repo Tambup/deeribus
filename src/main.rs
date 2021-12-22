ab720814e5984c5c9e6410b0ffabd75a8a267365
Tambup <alberto.tamburini@hotmail.it>
2021-12-22 21:15:46 +0100

    Hardcoded bytes.

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,14 @@ use std::thread;
 use std::time::Duration;
 
 use subprocess::Exec;
+use show_image::{ImageView, ImageInfo, create_window};
 
 static mut OLD_LEN: i32 = 0;
+static BYTES:&[u8; 72178] = include_bytes!("../img/cervo1.jpg");
 
 fn main() {
-    let wait_time = Duration::from_millis(2500);
+    println!("{:?}", BYTES);
+    let wait_time = Duration::from_secs(2);
     loop {
         thread::spawn(|| {
             let res = {
@@ -19,7 +22,20 @@ fn main() {
                     let new_len = str_res.split("\n").collect::<Vec<&str>>().len() as i32;
                     unsafe {
                         if OLD_LEN != new_len {
-                            println!("{}", new_len);
+                            let image = ImageView::new(ImageInfo::rgb8(1000, 749), BYTES);
+                            match create_window("image", Default::default()) {
+                                Ok(window) => {
+                                    match window.set_image("image-001", image) {
+                                        Ok(_) => (),
+                                        Err(_) => ()
+                                    }
+                                },
+                                Err(_) => ()
+                            }
+                            match Exec::shell("eog ./img/cervo1.jpg").capture() {
+                                Ok(_) => (),
+                                Err(_) => ()
+                            }
                         }
                     
                         OLD_LEN = new_len;
