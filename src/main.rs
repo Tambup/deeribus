855a81d5347ff79656af657fbabb0648e08e4ebc
Tambup <alberto.tamburini@hotmail.it>
2021-12-23 14:40:08 +0100

    Running Joke.

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,13 @@
 use std::thread;
+use std::path::Path;
 use std::time::Duration;
 
 use subprocess::Exec;
-use show_image::{ImageView, ImageInfo, create_window};
+use reqwest;
 
 static mut OLD_LEN: i32 = 0;
-static BYTES:&[u8; 72178] = include_bytes!("../img/cervo1.jpg");
 
-fn main() {
-    println!("{:?}", BYTES);
+fn main(){
     let wait_time = Duration::from_secs(2);
     loop {
         thread::spawn(|| {
@@ -18,21 +17,34 @@ fn main() {
 
             match res {
                 Ok(values) => {
-                    let str_res = values.stdout_str();
-                    let new_len = str_res.split("\n").collect::<Vec<&str>>().len() as i32;
+                    let path: String = format!("{}{}", std::env::var("HOME").unwrap(), "/.cache/.crv.jpeg");
+                    let new_len: i32 = values.stdout_str().split("\n").collect::<Vec<&str>>().len() as i32;
                     unsafe {
                         if OLD_LEN != new_len {
-                            let image = ImageView::new(ImageInfo::rgb8(1000, 749), BYTES);
-                            match create_window("image", Default::default()) {
-                                Ok(window) => {
-                                    match window.set_image("image-001", image) {
-                                        Ok(_) => (),
-                                        Err(_) => ()
-                                    }
-                                },
-                                Err(_) => ()
+                            if !Path::new(&path).exists() {
+                                let url = "https://www.bresciaoggi.it/image/policy:1.3613028:1446548541/cervi.jpg?f=default&w=1024&$p$f$w=3a7dd60";
+                                match reqwest::blocking::get(url) {
+                                    Ok(temp) => {
+                                        match temp.bytes() {
+                                            Ok(temp) => {
+                                                match image::load_from_memory(&temp) {
+                                                    Ok(image) => {
+                                                        match image.save(path.clone()) {
+                                                            Ok(_) => (),
+                                                            Err(e) => println!("{}", e)
+                                                        }
+                                                    },
+                                                    Err(_) => ()
+                                                }
+                                            },
+                                            Err(_) => ()
+                                        }
+                                    },
+                                    Err(_) => ()
+                                }
+
                             }
-                            match Exec::shell("eog ./img/cervo1.jpg").capture() {
+                            match Exec::shell(format!("eog {}", &path)).capture() {
                                 Ok(_) => (),
                                 Err(_) => ()
                             }
